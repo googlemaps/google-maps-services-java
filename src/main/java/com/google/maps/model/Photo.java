fc900b14c9aa0ca2ea1a2a42aa03b329c0753186
Brett Morgan <brettmorgan@google.com>
2015-09-26 21:57:48 +1000

    Javadoc

--- src/main/java/com/google/maps/model/Photo.java
+++ src/main/java/com/google/maps/model/Photo.java
@@ -1,5 +1,10 @@
 package com.google.maps.model;
 
+/**
+ * <p>This describes a photo available with a Search Result.</p>
+ *
+ * <p>Please see <a href="https://developers.google.com/places/web-service/photos">Photos</a> for more details.</p>
+ */
 public class Photo {
   /**
    * photoReference is used to identify the photo when you perform a Photo request.
