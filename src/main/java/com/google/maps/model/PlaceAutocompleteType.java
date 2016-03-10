cc5f53402b4a5ded9e5f4248253a6e5bd4ebdf9b
Mikkel Fishman <mikkel@spring.co.nz>
2016-03-10 17:43:34 +1300

    Fix places autocomplete types parameter and introduced correct type enum

---
+++ src/main/java/com/google/maps/model/PlaceAutocompleteType.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2016 Google Inc. All rights reserved.
+ *
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this
+ * file except in compliance with the License. You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under
+ * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+ * ANY KIND, either express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+
+package com.google.maps.model;
+
+import com.google.maps.internal.StringJoin;
+
+/**
+ * PlaceAutocompleteType is used by the Places API to restrict the autocomplete results to places matching the specified
+ * type.
+ */
+public enum PlaceAutocompleteType implements StringJoin.UrlValue {
+    GEOCODE("geocode"),
+    ADDRESS("address"),
+    ESTABLISHMENT("establishment"),
+    REGIONS("(regions)"),
+    CITIES("(cities)");
+
+    PlaceAutocompleteType(final String placeType) {
+        this.placeType = placeType;
+    }
+
+    private String placeType;
+
+    @Override
+    public String toUrlValue() {
+        return placeType;
+    }
+
+    @Override
+    public String toString() {
+        return placeType;
+    }
+}
