a739b8e710477e3637ade3dea52cf800c1c10f6e
Kazuki Nishiura <nkazuki@google.com>
2017-03-09 21:21:24 -0800

    Explicitly throw exception when unexpected (non GET or POST) request verb is specified. Before this change, such request ended up NullPointerExceptions.

--- src/main/java/com/google/maps/PendingResultBase.java
+++ src/main/java/com/google/maps/PendingResultBase.java
@@ -75,12 +75,15 @@ abstract class PendingResultBase<T, A extends PendingResultBase<T, A, R>,
           "'await', 'awaitIgnoreError' or 'setCallback' was already called.");
     }
     validateRequest();
-    if(config.requestVerb == "GET") {
-      delegate = context.get(config, responseClass, params);
-    } else if (config.requestVerb == "POST") {
-      delegate = context.post(config, responseClass, params);
+    switch (config.requestVerb) {
+      case "GET":
+        return delegate = context.get(config, responseClass, params);
+      case "POST":
+        return delegate = context.post(config, responseClass, params);
+      default:
+        throw new IllegalStateException(
+            String.format("Unexpected request method '%s'", config.requestVerb));
     }
-    return delegate;
   }
 
   protected abstract void validateRequest();
