4b502e707f68faa454ea86e691da95043de2162e
Bhalchandra Wadekar <BhalchandraSW@gmail.com>
2017-03-27 21:52:37 +0100

    Added format parameter

--- src/main/java/com/google/maps/StaticMapApiRequest.java
+++ src/main/java/com/google/maps/StaticMapApiRequest.java
@@ -42,10 +42,39 @@ public class StaticMapApiRequest extends PendingResultBase<StaticMapResult, Stat
         return param("size", width + "x" + height);
     }
 
+    /**
+     * (optional) affects the number of pixels that are returned. scale=2 returns twice as many
+     * pixels as scale=1 while retaining the same coverage area and level of detail (i.e. the
+     * contents of the map don't change). This is useful when developing for high-resolution
+     * displays, or when generating a map for printing. The default value is 1. Accepted values are
+     * 2 and 4 (4 is only available to Google Maps APIs Premium Plan customers.) See Scale Values
+     * for more information.
+     *
+     * @param scale
+     */
     public StaticMapApiRequest scale(int scale) {
         if (scale != 1 && scale != 2 && scale != 4) {
             throw new IllegalArgumentException("Accepted values are 1, 2, and 4.");
         }
         return param("scale", String.valueOf(scale));
+    }
+
+
+    /**
+     * (optional) defines the format of the resulting image. By default, the Google Static Maps API
+     * creates PNG images. There are several possible formats including GIF, JPEG and PNG types.
+     * Which format you use depends on how you intend to present the image. JPEG typically provides
+     * greater compression, while GIF and PNG provide greater detail. For more information, see
+     * Image Formats.
+     *
+     * @param format
+     */
+    public StaticMapApiRequest format(String format) {
+        if (!(format.equals("png") || format.equals("png8") || format.equals("png32")
+                || format.equals("gif") || format.equals("jpg") || format.equals("jpg-baseline"))) {
+            throw new IllegalArgumentException(
+                    "Accepted format values are png, png8, png32, gif, jpg, and jpg-baseline.");
+        }
+        return param("format", format);
     }
 }
