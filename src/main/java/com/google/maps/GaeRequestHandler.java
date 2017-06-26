1112caf87c2385819de11282fd75736237a9f2bf
Brett Morgan <brettmorgan@google.com>
2017-06-26 18:03:57 +1000

    Migrating to OkHttp3.

--- src/main/java/com/google/maps/GaeRequestHandler.java
+++ src/main/java/com/google/maps/GaeRequestHandler.java
@@ -22,6 +22,7 @@ import com.google.appengine.api.urlfetch.HTTPRequest;
 import com.google.appengine.api.urlfetch.URLFetchService;
 import com.google.appengine.api.urlfetch.URLFetchServiceFactory;
 import com.google.gson.FieldNamingPolicy;
+import com.google.maps.GeoApiContext.RequestHandler;
 import com.google.maps.internal.ApiResponse;
 import com.google.maps.internal.ExceptionsAllowedToRetry;
 import com.google.maps.internal.GaePendingResult;
@@ -41,6 +42,8 @@ import java.util.concurrent.TimeUnit;
 public class GaeRequestHandler implements GeoApiContext.RequestHandler {
   private static final Logger LOG = LoggerFactory.getLogger(GaeRequestHandler.class.getName());
   private final URLFetchService client = URLFetchServiceFactory.getURLFetchService();
+
+  /* package */ GaeRequestHandler() {}
 
   @Override
   public <T, R extends ApiResponse<T>> PendingResult<T> handle(String hostName, String url, String userAgent,
@@ -79,40 +82,44 @@ public class GaeRequestHandler implements GeoApiContext.RequestHandler {
     return new GaePendingResult<T, R>(req, client, clazz, fieldNamingPolicy, errorTimeout, maxRetries, exceptionsAllowedToRetry);
   }
 
+  /**
+   *  Builder strategy for constructing {@code GaeRequestHandler}.
+   */
+  public static class Builder implements GeoApiContext.RequestHandler.Builder {
 
-  @Override
-  public void setConnectTimeout(long timeout, TimeUnit unit) {
-    // TODO: Investigate if GAE URL Fetch Service supports setting connection timeout
-    throw new RuntimeException("setConnectTimeout not implemented for Google App Engine");
-  }
+    @Override
+    public void connectTimeout(long timeout, TimeUnit unit) {
+      // TODO: Investigate if GAE URL Fetch Service supports setting connection timeout
+      throw new RuntimeException("connectTimeout not implemented for Google App Engine");
+    }
 
-  @Override
-  public void setReadTimeout(long timeout, TimeUnit unit) {
-    // TODO: Investigate if GAE URL Fetch Service supports setting read timeout
-    throw new RuntimeException("setReadTimeout not implemented for Google App Engine");
-  }
+    @Override
+    public void readTimeout(long timeout, TimeUnit unit) {
+      // TODO: Investigate if GAE URL Fetch Service supports setting read timeout
+      throw new RuntimeException("readTimeout not implemented for Google App Engine");
+    }
 
-  @Override
-  public void setWriteTimeout(long timeout, TimeUnit unit) {
-    // TODO: Investigate if GAE URL Fetch Service supports setting write timeout
-    throw new RuntimeException("setWriteTimeout not implemented for Google App Engine");
-  }
+    @Override
+    public void writeTimeout(long timeout, TimeUnit unit) {
+      // TODO: Investigate if GAE URL Fetch Service supports setting write timeout
+      throw new RuntimeException("writeTimeout not implemented for Google App Engine");
+    }
 
-  @Override
-  public void setQueriesPerSecond(int maxQps) {
-    // TODO: Investigate if GAE URL Fetch Service supports setting qps
-    throw new RuntimeException("setQueriesPerSecond not implemented for Google App Engine");
-  }
+    @Override
+    public void queriesPerSecond(int maxQps) {
+      // TODO: Investigate if GAE URL Fetch Service supports setting qps
+      throw new RuntimeException("queriesPerSecond not implemented for Google App Engine");
+    }
 
-  @Override @Deprecated
-  public void setQueriesPerSecond(int maxQps, int minimumInterval) {
-    // TODO: Investigate if GAE URL Fetch Service supports setting qps
-    throw new RuntimeException("setQueriesPerSecond not implemented for Google App Engine");
-  }
+    @Override
+    public void proxy(Proxy proxy) {
+      // TODO: Investigate if GAE URL Fetch Service supports setting proxy
+      throw new RuntimeException("setProxy not implemented for Google App Engine");
+    }
 
-  @Override
-  public void setProxy(Proxy proxy) {
-    // TODO: Investigate if GAE URL Fetch Service supports setting proxy
-    throw new RuntimeException("setProxy not implemented for Google App Engine");
+    @Override
+    public RequestHandler build() {
+      return new GaeRequestHandler();
+    }
   }
 }
