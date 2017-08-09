23255bf4cd3efe6506d2a94272d16c4bf9066a9d
Andrew Janke <andrew@apjanke.net>
2017-08-08 23:12:23 -0400

    Javadoc: Rephrase to match Google Java Style Guide (classes A-C)
    This rephrases several Javadoc elements to section 7 of the Google Java Style Guide.

--- src/main/java/com/google/maps/model/AddressComponent.java
+++ src/main/java/com/google/maps/model/AddressComponent.java
@@ -24,21 +24,16 @@ package com.google.maps.model;
  * Developer's Guide</a> for more detail.
  */
 public class AddressComponent {
-  /**
-   * {@code longName} is the full text description or name of the address component as returned by
-   * the Geocoder.
-   */
+  /** The full text description or name of the address component as returned by the Geocoder. */
   public String longName;
 
   /**
-   * {@code shortName} is an abbreviated textual name for the address component, if available. For
-   * example, an address component for the state of Alaska may have a longName of "Alaska" and a
-   * shortName of "AK" using the 2-letter postal abbreviation.
+   * An abbreviated textual name for the address component, if available. For example, an address
+   * component for the state of Alaska may have a longName of "Alaska" and a shortName of "AK" using
+   * the 2-letter postal abbreviation.
    */
   public String shortName;
 
-  /**
-   * This indicates the type of each part of the address. Examples include street number or country.
-   */
+  /** Indicates the type of each part of the address. Examples include street number or country. */
   public AddressComponentType[] types;
 }
--- src/main/java/com/google/maps/model/AutocompletePrediction.java
+++ src/main/java/com/google/maps/model/AutocompletePrediction.java
@@ -16,8 +16,7 @@
 package com.google.maps.model;
 
 /**
- * AutocompletePrediction represents a single Autocomplete result returned from the Google Places
- * API Web Service.
+ * Represents a single Autocomplete result returned from the Google Places API Web Service.
  *
  * <p>Please see <a
  * href="https://developers.google.com/places/web-service/query#query_autocomplete_responses">Query
@@ -32,7 +31,7 @@ public class AutocompletePrediction {
   public String placeId;
 
   /**
-   * types is an array indicating the type of the address component.
+   * An array indicating the type of the address component.
    *
    * <p>Please see <a href="https://developers.google.com/places/supported_types">supported
    * types</a> for a list of types that can be returned.
@@ -40,37 +39,36 @@ public class AutocompletePrediction {
   public String types[];
 
   /**
-   * terms contains an array of terms identifying each section of the returned description (a
-   * section of the description is generally terminated with a comma). Each entry in the array has a
-   * value field, containing the text of the term, and an offset field, defining the start position
-   * of this term in the description, measured in Unicode characters.
+   * An array of terms identifying each section of the returned description. (A section of the
+   * description is generally terminated with a comma.) Each entry in the array has a value field,
+   * containing the text of the term, and an offset field, defining the start position of this term
+   * in the description, measured in Unicode characters.
    */
   public Term terms[];
 
   /**
-   * MatchedSubstring describes the location of the entered term in the prediction result text, so
-   * that the term can be highlighted if desired.
+   * Describes the location of the entered term in the prediction result text, so that the term can
+   * be highlighted if desired.
    */
   public static class MatchedSubstring {
 
-    /** length describes the length of the matched substring. */
+    /** The length of the matched substring. */
     public int length;
 
-    /** offset defines the start position of the matched substring. */
+    /** The start position of the matched substring. */
     public int offset;
   }
 
   public MatchedSubstring matchedSubstrings[];
 
   /**
-   * Term identifies each section of the returned description (a section of the description is
-   * generally terminated with a comma).
+   * Identifies each section of the returned description. (A section of the description is
+   * generally terminated with a comma.)
    */
   public static class Term {
 
     /**
-     * offset defines the start position of this term in the description, measured in Unicode
-     * characters.
+     * The start position of this term in the description, measured in Unicode characters.
      */
     public int offset;
 
