9b286a76518a4ddd684a17c41ae672a4020521e0
Andrew Janke <andrew@apjanke.net>
2017-08-10 00:41:56 -0400

    Javadoc: Rephrase to match Google Java Style Guide (classes H-O)
    This rephrases several Javadoc elements to conform with section 7 of the Google Java Style Guide.

--- src/main/java/com/google/maps/model/LocationType.java
+++ src/main/java/com/google/maps/model/LocationType.java
@@ -24,26 +24,25 @@ import com.google.maps.internal.StringJoin.UrlValue;
  */
 public enum LocationType implements UrlValue {
   /**
-   * {@code ROOFTOP} restricts the results to addresses for which we have location information
-   * accurate down to street address precision.
+   * Restricts the results to addresses for which we have location information accurate down to
+   * street address precision.
    */
   ROOFTOP,
 
   /**
-   * {@code RANGE_INTERPOLATED} restricts the results to those that reflect an approximation
-   * (usually on a road) interpolated between two precise points (such as intersections). An
-   * interpolated range generally indicates that rooftop geocodes are unavailable for a street
-   * address.
+   * Restricts the results to those that reflect an approximation (usually on a road) interpolated
+   * between two precise points (such as intersections). An interpolated range generally indicates
+   * that rooftop geocodes are unavailable for a street address.
    */
   RANGE_INTERPOLATED,
 
   /**
-   * {@code GEOMETRIC_CENTER} restricts the results to geometric centers of a location such as a
-   * polyline (for example, a street) or polygon (region).
+   * Restricts the results to geometric centers of a location such as a polyline (for example, a
+   * street) or polygon (region).
    */
   GEOMETRIC_CENTER,
 
-  /** {@code APPROXIMATE} restricts the results to those that are characterized as approximate. */
+  /** Restricts the results to those that are characterized as approximate. */
   APPROXIMATE,
 
   /**
