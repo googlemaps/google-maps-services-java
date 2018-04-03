c1179039ef2cc3d64d51a2e266784cce58f05e7e
Brett Morgan <brett.morgan@gmail.com>
2018-04-03 16:32:48 +1000

    Making height and width explicit

--- src/test/java/com/google/maps/StaticMapsApiTest.java
+++ src/test/java/com/google/maps/StaticMapsApiTest.java
@@ -6,34 +6,28 @@ import static org.junit.Assert.assertNotNull;
 import com.google.maps.model.Size;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import javax.imageio.ImageIO;
-import org.junit.Before;
 import org.junit.Test;
 
 public class StaticMapsApiTest {
 
-  BufferedImage bufferedImage;
-
-  @Before
-  public void setUp() throws IOException {
-    bufferedImage = new BufferedImage(300, 300, BufferedImage.TYPE_INT_ARGB);
-  }
+  private final int WIDTH = 640;
+  private final int HEIGHT = 480;
 
   @Test
   public void testGetSydneyStaticMap() throws Exception {
-    try (LocalTestServerContext sc = new LocalTestServerContext(bufferedImage)) {
+    try (LocalTestServerContext sc =
+        new LocalTestServerContext(new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_ARGB))) {
 
-      StaticMapsRequest req = StaticMapsApi.newRequest(sc.context, new Size(300, 300));
+      StaticMapsRequest req = StaticMapsApi.newRequest(sc.context, new Size(WIDTH, HEIGHT));
       req.center("Google Sydney");
       req.zoom(16);
-      ImageResult result = req.await();
-      ByteArrayInputStream input = new ByteArrayInputStream(result.imageData);
-      BufferedImage img = ImageIO.read(input);
+      ByteArrayInputStream bais = new ByteArrayInputStream(req.await().imageData);
+      BufferedImage img = ImageIO.read(bais);
 
       assertNotNull(img);
-      assertEquals(300, img.getWidth());
-      assertEquals(300, img.getHeight());
+      assertEquals(WIDTH, img.getWidth());
+      assertEquals(HEIGHT, img.getHeight());
     }
   }
 }
